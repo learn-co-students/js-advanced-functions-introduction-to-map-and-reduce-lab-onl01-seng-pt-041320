a329a134cfc8b9201c8aa8969a4d65aa8a583699
christinezosche <christine.zosche@gmail.com>
2020-11-18 12:47:15 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (const element of sourceArray){
+        newArray.push(-element)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = []
+    for (const element of sourceArray){
+        newArray.push(element)
+    }
+    return newArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    for (const element of sourceArray){
+        newArray.push(element*2)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    for (const element of sourceArray){
+        newArray.push(element*element)
+    }
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        startingPoint += sourceArray[i];
+    }
+    return startingPoint
+}
+
+function reduceToAllTrue(sourceArray) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i] === false) {
+            newArray.push(sourceArray[i])
+        }
+    }
+    if (newArray[0] === undefined) {
+        return true
+    }
+    else {
+    return false
+    }
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i] === true) {
+            newArray.push(sourceArray[i])
+        }
+    }
+    if (newArray[0] === undefined) {
+        return false
+    }
+    else {
+    return true
+    }
+}
