dee595d1d2e9bc743f396918c1c9ee3ae6c04d5d
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-28 01:38:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map((x) => {return (x * -1)})
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map((x) => {return (x)})
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map((x) => {return (x * 2)})
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map((x) => {return (x ** 2)})
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    const reducer = (sum, val) => sum + val;
+    return sourceArray.reduce(reducer, startingPoint);
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) {return false}
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) {return true}
+    }
+    return false
+}
